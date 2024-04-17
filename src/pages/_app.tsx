6c58a6f1e74888baa0c4e32824aee296d1055bca
Brambs <gabriel128.br@gmail.com>
2024-04-17 13:30:32 -0300

    Interface Styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,17 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { globalStyles } from '@/styles/global'
+import { Container, Header } from '@/styles/pages/app'
+import { AppProps } from 'next/app'
+import Image from 'next/image'
+import logoImg from '../assets/logo.svg'
 
-export default MyApp
+globalStyles()
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,29 @@
+import { HomeContainer, Product } from '@/styles/pages/home'
+import Image from 'next/image'
+import camiseta1 from '../assets/Camisa-Maratona.png'
+import camiseta2 from '../assets/IgniteLab-T-shirt.png'
+import camiseta3 from '../assets/Igniter-abord-2-t-shirt.png'
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
 }
