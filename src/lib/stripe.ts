334be8c3e0a8dc0296e15c8675cf093eb6cc03c2
Brambs <gabriel128.br@gmail.com>
2024-04-22 19:50:13 -0300

    Env variables fixed
    Thanks god

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRECT_KEY, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2024-04-10',
   appInfo: {
     name: 'Ignite Shop'
