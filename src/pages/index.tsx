d65de0beb7220d38a6bbf3c00c95d360736cd053
Brambs <gabriel128.br@gmail.com>
2024-04-24 11:44:11 -0300

    Price formating

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import camiseta3 from '../assets/Igniter-abord-2-t-shirt.png'
 import { GetServerSideProps, GetStaticProps } from 'next'
 import Stripe from 'stripe'
 import { stripe } from '@/lib/stripe'
+import Link from 'next/link'
 
 interface HomeProps {
   products: {
@@ -20,14 +21,16 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer>
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
+          <Link href="" key={product.id}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
@@ -46,12 +49,16 @@ export const getServerSideProps: GetStaticProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount / 100
+      price: new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+      }).format(price.unit_amount / 100)
     }
   })
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2
   }
 }
